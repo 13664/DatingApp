00473b6396ad7facce4ce33c5e022bef1bc079d7
Nigina <wiut13664@gmail.com>
2024-01-15 11:58:05 +0500

    end of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -2,18 +2,20 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(options =>
 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
-builder.Services.AddControllers();
+builder.Services.AddCors();
 
 
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 
 app.UseHttpsRedirection();
